24d2190732a7f3289e0ae2b993b56b2901d97028
roxanaMa <daniela.mardare@gmail.com>
2021-03-24 17:15:09 +0200

    Initial Commit

---
+++ WarCardGame/ContentView.swift
@@ -0,0 +1,101 @@
+//
+//  ContentView.swift
+//  WarCardGame
+//
+//  Created by Roxy Mardare on 03.03.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State private var playerCard = "card5"
+    @State private var cpuCard = "card9"
+    @State private var playerScore = 0
+    @State private var cpuScore = 0
+    
+    var body: some View {
+       
+        ZStack {
+            
+            Image("background")
+                .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+           
+        VStack {
+            Spacer()
+            Image("logo")
+            Spacer()
+            HStack {
+                Spacer()
+                Image(playerCard)
+                Spacer()
+                Image(cpuCard)
+                Spacer()
+            }
+          Spacer()
+            Button (action: {
+                //Generate a random number
+                let playerRand = Int.random(in: 2...14)
+                let cpuRand = Int.random(in: 2...14)
+                
+                //Update the cards
+                playerCard = "card" + String(playerRand)
+                cpuCard = "card" + String(cpuRand)
+                
+                //Update the score
+                if playerRand > cpuRand {
+                    playerScore += 1
+                }
+                else if playerRand == cpuRand {
+                    playerScore += 1
+                    cpuScore += 1
+                }
+                else {
+                cpuScore += 1
+                }
+                
+            }, label: {
+                Image ("dealbutton")
+            })
+           Spacer()
+            HStack{
+                Spacer()
+                VStack{
+                    
+                    Text ("Player")
+                        .font(.headline)
+                        .foregroundColor(Color.white)
+                        .padding(.bottom, 10.0)
+                    Text (String(playerScore))
+                        .font(.largeTitle)
+                        .foregroundColor(Color.white)
+                    
+                }
+                Spacer()
+                VStack{
+                    Text("CPU")
+                        .font(.headline)
+                        .foregroundColor(Color.white)
+                        .padding(.bottom, 10.0)
+                    Text (String(cpuScore))
+                        .font(.largeTitle)
+                        .foregroundColor(Color.white)
+                        }
+                Spacer()
+            }
+            Spacer()
+                }
+        
+            }
+        }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            ContentView()
+                .previewDevice("iPhone 11")
+        }
+        
+    }
+}
---
+++ WarCardGame/WarCardGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  WarCardGameApp.swift
+//  WarCardGame
+//
+//  Created by Roxy Mardare on 03.03.2021.
+//
+
+import SwiftUI
+
+@main
+struct WarCardGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
